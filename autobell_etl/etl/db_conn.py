ec570a52436c02fb40be28669192018bf91245e8
John Mastronardo <73623041+jvmastro@users.noreply.github.com>
2022-05-19 12:15:43 -0400

    heroku connection module

---
+++ autobell_etl/etl/db_conn.py
@@ -0,0 +1,20 @@
+import subprocess
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+class HerokuDBConnection:
+    
+    def __init__(self, **kwargs):
+        self.DB_URI = subprocess.run(["heroku", "config:get","DATABASE_URL", "--app", "autobell-beta"], stdout=subprocess.PIPE).stdout.decode('utf-8').replace('postgres','postgresql').strip()
+
+
+        # Now create the engine
+        self.engine = create_engine(self.DB_URI, echo=True, future=True)
+        # Make the session maker
+        self.session_maker = sessionmaker(bind=self.engine)
+
+    @property
+    def session(self):
+        """Return a session as a property"""
+        return self.session_maker()
